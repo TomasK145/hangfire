b7ff5b4c68ccde54c363fb66ead5cdc174685150
Tomas Krchnak <thomas.krchnak@gmail.com>
2018-02-17 14:06:21 +0100

    hangfire test 2

--- HangfireConsoleApp/Program.cs
+++ HangfireConsoleApp/Program.cs
@@ -9,16 +9,35 @@ namespace HangfireConsoleApp
         {
             GlobalConfiguration.Configuration.UseColouredConsoleLogProvider();
             GlobalConfiguration.Configuration.UseSqlServerStorage("Hangfire");
-
+            
             using (var server = new BackgroundJobServer())
             {
+                string value = "NA";
                 Console.WriteLine("Hangfire server started...");
 
-                BackgroundJob.Enqueue(() => Console.WriteLine("Hello world from hangfire."));
-            }
+                BackgroundJob.Enqueue(() => Calculate("test1"));
+                Console.WriteLine("value: " + value);
 
+                BackgroundJob.Enqueue(() => Calculate("test2"));
+                Console.WriteLine("value: " + value);
+
+                BackgroundJob.Enqueue(() => Calculate("test2"));
+                Console.WriteLine("value: " + value);
+
+
+            }            
             Console.WriteLine("Done");
             Console.ReadLine();
+        }
+
+        public static void Calculate(string testName)
+        {
+            int result = 0;
+            for (int i = 0; i <= 1000; i++)
+            {
+                result += i;
+            }
+            Console.WriteLine($"test: {testName} - result: {result}");
         }
     }
 }
